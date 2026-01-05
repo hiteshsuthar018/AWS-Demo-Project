c5987a4f4178f380e74d388716ce0331b36293d6
hitesh906 <hitesh@ohmgpt.com>
2026-01-05 09:57:14 +0530

    added todos

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ import ip from "ip";
 const app = express();
 
 
-app.get("/ping",(req,res)=>{
-    for(let i=0;i<10000000000;i++){
-        
-    }
-    res.json({
-        message:"pong",
-        serverAddress:ip.address() 
-    })
+app.get("/",(req,res)=>{
+
+   res.json([
+  { id: 1, title: "Wake up" },
+  { id: 2, title: "Brush teeth" },
+  { id: 3, title: "Exercise" },
+  { id: 4, title: "Take a shower" },
+  { id: 5, title: "Have breakfast" },
+  { id: 6, title: "Check emails" },
+  { id: 7, title: "Work on project" },
+  { id: 8, title: "Lunch break" },
+  { id: 9, title: "Read a book" },
+  { id: 10, title: "Sleep" }
+]);
+
 })
 
 app.listen(3000,()=>{
